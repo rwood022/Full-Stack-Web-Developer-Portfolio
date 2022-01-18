a00bb5720d8206a8dcd455989b6e006c94a33dfb
rwood022 <93175265+rwood022@users.noreply.github.com>
2022-01-18 00:23:31 -0500

    added button links to html

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,26 @@
 var nuMu = $("nuMu");
-var webDeveloper = $("webDeveloper");
+var resume = $("resume");
+var linkedIn = $("linkedIn");
+var gitHub = $("gitHub");
 
 
 
+//code from codeofaninja.com, 
+$("#nuMu").click(function() {
+    window.open("https://rwood022.github.io/YouTube_Music_FInder/");
+});
 
-nuMu.on("click", function() {
-    
-})
+
+$("resume").click(function() {
+    document.location.href = WebDeveloperResume.pdf;
+});
+
+
+$("linkedIn").click(function(){
+    window.open("www.linkedin.com/in/dr-rebecca-wood-7a279846");
+});
+
+
+$("gitHub").click(function() {
+    window.open(`https://github.com/rwood022`);
+});
